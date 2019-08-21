2105af9a603cfe593fd4ff06525a63c3ab541e51
OTeeEnabor <tobienabor@gmail.com>
2019-08-21 20:27:44 +0200

    Challenge completed.

--- challenge_3.php
+++ challenge_3.php
@@ -18,12 +18,30 @@ Once your statement is set up you must be able to get different outputs by only 
     <title>Challenge-3</title>
 </head>
 <body>
-    <form action="<?php echo htmlscpecialchars($_SERVER['PHP_SELF']) ?>" role="form" method="post">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" role="form" method="post">
 
     <label> Please enter your employee name : 
     <input type ="text" name="employee" placeholder="please enter your first name">
     </label>
+
+    <input type="submit" name="submitName">
     </form>
+
+    <?php
+    $employee_name =array('Harry','Fela','Mac','Zombie','Shadow');
+    if(isset($_POST['submitName'])&&(isset($_POST['employee']))){
+
+        $input =ucfirst(strtolower($_POST['employee']));
+        if (in_array($input,$employee_name)){
+            echo "Hello " . $input . "... <br>Thank you for coming in today to give your precious hours, get to work:)";
+        }
+        else{
+            echo "YOU DONT WORK HERE!!!...but you could if you are willing to sacrifice your hours for some money.";
+        }
+
+
+    }
+    ?>
 </body>
 </html>
 
